480860bc3a403a878fca07708cfe7b38848fbde8
tegarfauzan <tegarfauzanfirdaus@gmail.com>
2024-06-10 13:09:01 +0700

    make a Park Angga Page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom': '0px 10px 20px 0px #0706250F'
+      },
+      fontFamily: {
+        'plus-jakarta-sans': ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
