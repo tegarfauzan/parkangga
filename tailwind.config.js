7db4e526948d0c8764ddf08310847e39e25737bb
tegarfauzan <tegarfauzanfirdaus@gmail.com>
2024-06-12 09:04:06 +0700

    Make A Dicsover Page in discover.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'custom': '0px 10px 20px 0px #0706250F'
+        custom: "0px 10px 20px 0px #0706250F",
+        customSearch: "0px 10px 20px 0px #0706250F",
       },
       fontFamily: {
-        'plus-jakarta-sans': ['Plus Jakarta Sans', 'sans-serif'],
+        "plus-jakarta-sans": ["Plus Jakarta Sans", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-
+};
